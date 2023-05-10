f3abdf8b0353228aa15968d86c92f65d12913681
Jonathan <jonathan.max.annett@gmail.com>
2023-05-10 22:43:37 +1000

    Removed unused function previously used to install BITS downloader

--- make-portable.ps1
+++ make-portable.ps1
@@ -32,37 +32,7 @@ $ptzdelay_zip = "$bundled\ptz-move-delay.zip"
 $ndi_zip =  "$downloads\obs-ndi-4.11.1-windows-x64.zip"    
 
 
-function LoadModule ($m) {
 
-    # If module is imported say that and do nothing
-    if (Get-Module | Where-Object {$_.Name -eq $m}) {
-        write-host "Module $m is already imported."
-        return $true
-    }
-    else {
-
-        # If module is not imported, but available on disk then import
-        if (Get-Module -ListAvailable | Where-Object {$_.Name -eq $m}) {
-            Import-Module $m -Verbose
-            return $true
-        }
-        else {
-
-            # If module is not imported, not available on disk, but is in online gallery then install and import
-            if (Find-Module -Name $m | Where-Object {$_.Name -eq $m}) {
-                Install-Module -Name $m -Force -Verbose -Scope CurrentUser
-                Import-Module $m -Verbose
-                return $true
-            }
-            else {
-
-                # If the module is not imported, not available and not in the online gallery then abort
-                write-host "Module $m not imported, not available and not in an online gallery, exiting."
-                return $false
-            }
-        }
-    }
-}
 
  
 
